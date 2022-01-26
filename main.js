ce5cd9629ad6408dc804400a740ae9ff02eefdf5
Tom Porter <tom.port@yahoo.co.uk>
2022-01-26 14:49:51 +0000

    base functions &  Calculator class completed. The calculator broadly working in this state.

--- main.js
+++ main.js
@@ -12,28 +12,59 @@ class Calculator {
     }
     
     delete() {
-    
+    this.currentOperand = this.currentOperand/toString().slice(0, -1)
     }
     
     appendNumber(number) {
-    
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
     }
     
     selectOperation(operation) {
-    
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.getResult()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
     }
     
     getResult() {
-    
+        let calculation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                calculation = prev + current
+                break
+             case '-':
+                 calculation = prev - current
+                 break
+            case '÷':
+                calculation = prev / current
+                break
+            case '*':
+                calculation = prev * current
+                break
+            default:
+                return
+        }
+        this.currentOperand = calculation
+        this.operation = undefined
+        this.previousOperand = ''
+                   
     }
 
     updateDisplay() {
-      
+      this.currentOperandText.innerText = this.currentOperand
+      this.previousOperandText.innerText = this.previousOperand
     }
 }
 
 
-
+// const variables & querySelectors from HTML
 
 
 const numberButtons = document.querySelectorAll('[data-number]')
@@ -46,6 +77,8 @@ const currentOperandText = document.querySelector('[data-current-operand]')
 
 const calculator = new Calculator(previousOperandText, currentOperandText)
 
+// button functionality 
+
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
       calculator.appendNumber(button.innerText)  
@@ -53,3 +86,24 @@ numberButtons.forEach(button => {
     })
 })
 
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.selectOperation(button.innerText)  
+      calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.getResult()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
